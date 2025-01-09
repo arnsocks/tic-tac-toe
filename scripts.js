d36eec20e7e1137b5ccb78d3c0d2b3cca429d448
James Arn <132308884+arnsocks@users.noreply.github.com>
2025-01-08 20:28:57 -0500

    Implement functional console game without win detection.

--- scripts.js
+++ scripts.js
@@ -13,10 +13,9 @@ const GameBoard = function () {
   const getBoard = () => board;
 
   const placeToken = (cell, player) => {
-    // TODO - Make sure given cell is not taken
+    if (board[cell.row][cell.column].getValue() !== 0) return; // Cannot play in a non-empty full cell.
 
-    // put the current player's token in the given cell
-    board[cell.row][cell.column].addToken(player);
+    board[cell.row][cell.column].addToken(player.token);
   };
 
   const printBoard = () => {
@@ -29,7 +28,6 @@ const GameBoard = function () {
     placeToken,
     printBoard
   };
-
 };
 
 function Cell() {
@@ -44,6 +42,89 @@ function Cell() {
     addToken,
     getValue
   };
+};
+
+const GameController = function (
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = GameBoard();
+  const players = [
+    {
+      name: playerOneName,
+      token: "X"
+    },
+    {
+      name: playerTwoName,
+      token: "O"
+    }
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  }
+  const getActivePlayer = () => activePlayer;
+  
+  const printNewRound = () => {
+    console.log(activePlayer.name + "'s turn!");
+    board.printBoard();
+  }
+
+  const playRound = (cell) => {
+
+    // Don't accept a non-empty cell
+    if (board.getBoard()[cell.row][cell.column].getValue() !== 0) {
+      console.log("You cannot play in a non-empty cell");
+      printNewRound();
+      return;
+    } 
+
+    board.placeToken(cell, getActivePlayer());
+    
+    
+
+    // TODO - Logic to determine if there is an endstate
+    // if (checkForWinner(board.getBoard())) return;
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  // const checkForWinner = (board) => {
+  //   //Check each of the rows
+  //   for (let i = 0; i < board.length; i++) {
+  //     const firstToken = board[i][0].getValue();
+  //     const matchesFirstToken = (cell) => cell.getValue() === firstToken;
+  //     if (!firstToken) return;
+  //     console.log({firstToken});
+  //     if (board[i].every( (cell) => matchesFirstToken(cell))) {
+  //       console.log(`${getActivePlayer().name} wins!`)
+  //       return true;
+  //     }
+  //   };
+
+  //   //Check each of the columns
+  //   //for (let i = 0; i < board.length
+  // };
+  
+  printNewRound();
+
+
+  return {
+    getActivePlayer,
+    playRound,
+    getBoard: board.getBoard
+  };
 }
 
-game = GameBoard();
+game = GameController();
+
+
+//run test game
+
+game.playRound({row:0, column:0}) // Player 1 first turn
+game.playRound({row:1, column:0}) // Player 2 first turn
+game.playRound({row:0, column:1}) // Player 1 2nd turn
+game.playRound({row:1, column:1}) // Player 2 2nd turn
+game.playRound({row:0, column:2}) // Player 1 first turn - PLAYER 1 SHOULD WIN!
