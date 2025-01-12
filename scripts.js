6f59762e2cb3a4ce766acc0dce57fd7ca0b00373
James Arn <132308884+arnsocks@users.noreply.github.com>
2025-01-12 12:00:30 -0500

    Refactor the game logic to reduce global scope.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const GameBoard = function () {
+const GameBoard = (function () {
   const rows = 3;
   const columns = 3;
   let board = [];
@@ -13,8 +13,6 @@ const GameBoard = function () {
   const getBoard = () => board;
 
   const placeToken = (cell, player) => {
-    if (board[cell.row][cell.column].getValue() !== 0) return; // Cannot play in a non-empty full cell.
-
     board[cell.row][cell.column].addToken(player.token);
   };
 
@@ -23,44 +21,42 @@ const GameBoard = function () {
     console.log(boardWithCellValues);
   };
 
+  function Cell() {
+    let value = 0;
+    const addToken = (player) => {
+      value = player;
+    }
+
+    const getValue = () => value;
+
+    return {
+      addToken,
+      getValue
+    };
+  };
+
   return {
     getBoard,
     placeToken,
     printBoard
   };
-};
+})();
 
-function Cell() {
-  let value = 0;
-  const addToken = (player) => {
-    value = player;
-  }
-
-  const getValue = () => value;
-
-  return {
-    addToken,
-    getValue
-  };
-};
-
-const GameController = function (
-  playerOneName = "Player One",
-  playerTwoName = "Player Two"
-) {
-  const gameBoard = GameBoard();
+const GameController = (() => {
+  //const gameBoard = GameBoard();
   const players = [
     {
-      name: playerOneName,
+      name: 'Player One',
       token: "X"
     },
     {
-      name: playerTwoName,
+      name: 'Player Two',
       token: "O"
     }
   ];
 
   let activePlayer = players[0];
+  
 
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
@@ -69,33 +65,37 @@ const GameController = function (
   
   const printNewRound = () => {
     console.log(activePlayer.name + "'s turn!");
-    gameBoard.printBoard();
+    GameBoard.printBoard();
   }
 
   const playRound = (cell) => {
     let gameResult = '';
     // Don't accept a non-empty cell
-    // if (gameBoard.getBoard()[cell.row][cell.column].getValue() !== 0) {
-    //   console.log("You cannot play in a non-empty cell");
-    //   printNewRound();
-    //   return;
-    // } 
+    if (GameBoard.getBoard()[cell.row][cell.column].getValue() !== 0) {
+      console.log("You cannot play in a non-empty cell");
+      printNewRound();
+      return;
+    } 
 
-    gameBoard.placeToken(cell, getActivePlayer());
+    GameBoard.placeToken(cell, getActivePlayer());
     printNewRound();
     
-    gameResult = checkResult(gameBoard.getBoard());
+    gameResult = checkResult(GameBoard.getBoard());
     if (gameResult === 'win') {
+      console.log(`${activePlayer.name} wins!`)
       return `${activePlayer.name} wins!`
     } else if (gameResult === 'draw') {
+      console.log(`It's a draw`);
       return `It's a draw`;
-    };
-
-    switchPlayerTurn();
+    } else switchPlayerTurn(); // If there's no result keep playing
   };
 
-  function checkResult(board) {
+  const setPlayerName = (index, playerName) => {
+    players[index].name = playerName;
+  }
   
+  function checkResult(board) {
+    
     const boardSize = board.length;
 
     // If every item on the line matches and is non-zero then the line is a win
@@ -158,36 +158,44 @@ const GameController = function (
     // if every cell is non zero and there's no winner then it's a draw
     if (board.flat().every( (cell) => cell.getValue())) return 'draw';
   };
-  
-  printNewRound();
 
+  printNewRound(); //Print the board for the first round
 
   return {
     getActivePlayer,
     playRound,
-    getBoard: gameBoard.getBoard
+    setPlayerName
   };
-}
+})();
 
-game = GameController();
+const ScreenController = (() => {
+  // DECLARE DOM objects into variables
 
+  // ** METHODS **
+
+  // Update Screen
+
+  // Handle Click Events
+
+
+})();
 
 //run test game
 
-// game.playRound({row:0, column:2}) // Player 1 first turn
-// game.playRound({row:1, column:0}) // Player 2 first turn
-// game.playRound({row:1, column:1}) // Player 1 2nd turn
-// game.playRound({row:1, column:2}) // Player 2 2nd turn
-// game.playRound({row:2, column:0}) // Player 1 first turn - PLAYER 1 SHOULD WIN!
+GameController.playRound({row:0, column:2}) // Player 1 first turn
+GameController.playRound({row:1, column:0}) // Player 2 first turn
+GameController.playRound({row:1, column:1}) // Player 1 2nd turn
+GameController.playRound({row:1, column:2}) // Player 2 2nd turn
+GameController.playRound({row:2, column:0}) // Player 1 first turn - PLAYER 1 SHOULD WIN!
 
 
-//test game should be a draw
-game.playRound({row:0, column:0}) // Player 1 first turn
-game.playRound({row:0, column:1}) // Player 2 first turn
-game.playRound({row:1, column:0}); // 1
-game.playRound({row:1, column:1}); // 2
-game.playRound({row:2, column:2}); // 1
-game.playRound({row:2, column:0}); // 2
-game.playRound({row:2, column:1}); // 1
-game.playRound({row:1, column:2}); // 2
-game.playRound({row:0, column:2}); // 1
+// //test game should be a draw
+// game.playRound({row:0, column:0}) // Player 1 first turn
+// game.playRound({row:0, column:1}) // Player 2 first turn
+// game.playRound({row:1, column:0}); // 1
+// game.playRound({row:1, column:1}); // 2
+// game.playRound({row:2, column:2}); // 1
+// game.playRound({row:2, column:0}); // 2
+// game.playRound({row:2, column:1}); // 1
+// game.playRound({row:1, column:2}); // 2
+// game.playRound({row:0, column:2}); // 1
